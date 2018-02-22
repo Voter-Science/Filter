d7b1bc782b0d9bdbc9dfd3af008e3c8e47d8d01e
Rita Gaherwar <rita@ranium.in>
2018-02-22 17:18:30 +0530

    Added functionality to draw and filter Polygon

--- src/pluginmain.ts
+++ src/pluginmain.ts
@@ -22,6 +22,7 @@ import { ColumnStats } from './columnStats'
 import { HashCount } from './hashcount'
 
 declare var $: any; // external definition for JQuery
+declare var MarkerClusterer: any; // external definition
 
 // Provide easy error handle for reporting errors from promises.  Usage:
 //   p.catch(showError);
@@ -29,8 +30,24 @@ declare var clearError: () => void; // error handler defined in index.html
 declare var showError: (error: any) => void; // error handler defined in index.html
 declare var google: any;
 declare var HeatmapOverlay: any;
+declare var randomColor: any; // from randomColor()
 
-// We create a "virtual column" for the polyons defined in this sheet. 
+// $$$ get from TRC
+interface IGeoPoint {
+    Lat: number;
+    Long: number;
+}
+
+// List of partitions that we created.
+interface IPartition {
+    sheetId: string;
+    name: string;
+    dataId: string; // data for the polygon
+    polygon: any; // google maps polygon
+    infoWindow: any; // google Info window for displaying polygon name
+}
+
+// We create a "virtual column" for the polyons defined in this sheet.
 const PolygonColumnName : string = "$IsInPolygon";
 
 export class MyPlugin {
@@ -49,6 +66,23 @@ export class MyPlugin {
     private _optionFilter: any = [];
 
     private _lastResults: QueryResults; // Results of last query
+    private _opts: plugin.IPluginOptions;
+    private _map: any;
+    private _markers: any[]; // map markers; { recId: marker }
+    private _partitions: { [id: string]: IPartition; }; // Map sheetId --> IPartition
+    private _markerCluster: any;
+
+    // $$$ find a way to avoid this.
+    private static _pluginId: string = "Geofencing.Beta";
+
+    // $$$ Move to PluginOptionsHelper?
+    private getGotoLinkSheet(sheetId: string): string {
+        if (this._opts == undefined) {
+            return "/"; // avoid a crash
+        }
+        return this._opts.gotoUrl + "/" + sheetId + "/" +
+            MyPlugin._pluginId + "/index.html";
+    }
 
     public static BrowserEntryAsync(
         auth: plugin.IStart,
@@ -79,6 +113,9 @@ export class MyPlugin {
     public constructor(p: plugin.PluginClient) {
         this._sheet = new trcSheet.SheetClient(p.HttpClient, p.SheetId);
         this._polyHelper = new trcPoly.PolygonHelper(this._sheet);
+
+        this._markers = [];
+        this._partitions = {};
     }
 
 
@@ -164,10 +201,10 @@ export class MyPlugin {
         });
     };
 
-    // Map of polygon names (which are used in the query UX) to 
+    // Map of polygon names (which are used in the query UX) to
     // a dataId (which is used in the query expression)
     private _polyName2IdMap: any = {};
-    public static LatestPolyMap: any; // expose the map outside this class. 
+    public static LatestPolyMap: any; // expose the map outside this class.
 
     // Collect stats on sheets. Namely which columns, and possible values.
     private getStats(): Promise<void> {
@@ -182,7 +219,7 @@ export class MyPlugin {
 
                         return iter.ForEach((item) => {
                             var dataId = item.DataId; // unique for API
-                            var name = item.Name; // friendly version 
+                            var name = item.Name; // friendly version
 
                             if (this._polyName2IdMap == null) {
                                 this._polyName2IdMap = {};
@@ -386,12 +423,12 @@ export class MyPlugin {
                 '<p>' + addresses[i] + ', ' + cities[i] + '</p>' +
                 '</div>';
 
-            google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            google.maps.event.addListener(marker, 'click', (function (marker, i, infoContent) {
                 return function () {
                     infowindow.setContent(infoContent);
                     infowindow.open(map, marker);
                 }
-            })(marker, i));
+            })(marker, i, infoContent));
             bounds.extend(latLng);
         }
         map.fitBounds(bounds);       // auto-zoom
@@ -407,9 +444,17 @@ export class MyPlugin {
         };
 
         // standard map
-        var map = new google.maps.Map(document.getElementById(containerId), myOptions);
+        this._map = new google.maps.Map(document.getElementById(containerId), myOptions);
 
-        return map;
+        this._markerCluster = new MarkerClusterer(
+            this._map,
+            [], { imagePath: 'https://cdn.rawgit.com/googlemaps/js-marker-clusterer/gh-pages/images/m' });
+
+        this.FinishInit(() => { } );
+
+        this.initDrawingManager(this._map); // adds drawing capability to map
+
+        return this._map;
     }
 
     public onCreateHeatMap(): void {
@@ -543,8 +588,8 @@ export class MyPlugin {
 
                     if (columnName == PolygonColumnName)
                     {
-                        // This represents the polygons. 
-                        // Polygon comparison is either in or out. 
+                        // This represents the polygons.
+                        // Polygon comparison is either in or out.
                         operators = [
                             JQBOperator.Equal, JQBOperator.NotEqual
                             ];
@@ -556,8 +601,6 @@ export class MyPlugin {
                         values = getPossibleValues;
                     }
                 }
-
-                var options: any = [];
 
                 var fields: any = {
                     'id': columnName,
@@ -607,6 +650,375 @@ export class MyPlugin {
 
         this.showFilterResult(filter);
     }
+
+    // Performance note: Do all the UI upfront and then do all the map updates (add polygons, etc).
+    // It's a *huge* performance penalty to interleave them because it prevents map rendering
+    // from being batched up and done all at once.
+    private BuildPartitions(
+        children: trcSheet.IGetChildrenResultEntry[],
+        callback: () => void
+    ): void {
+        var _missing = 0;
+        var _extra: any = {};
+        var remaining = children.length;
+
+        // Second pass. After we collect all the IO, then update the map.
+        var next = () => {
+            remaining--;
+            if (remaining == 0) {
+                for (var sheetId in this._partitions) {
+                    var partition = this._partitions[sheetId];
+                    var polySchema = _extra[sheetId].polySchema;
+                    var count = _extra[sheetId].count;
+
+                    partition.polygon = MyPlugin.newPolygon(polySchema, this._map);
+                    this.physicallyAddPolygon(
+                        partition.name,
+                        partition.polygon,
+                        count,
+                        partition.sheetId);
+                }
+
+
+                callback();
+            }
+        };
+
+        if (children.length == 0) {
+            remaining = 1;
+            next();
+            return;
+        }
+
+        // Do the first pass for all IO.
+        // Dispatch IO in parallel.
+        for (var i = 0; i < children.length; i++) {
+            var _child = children[i];
+
+            ((child: trcSheet.IGetChildrenResultEntry) => {
+                var sheetId = child.Id;
+                var childSheet = this._sheet.getSheetById(sheetId);
+                childSheet.getInfoAsync().then(childInfo => {
+                    var filter = child.Filter;
+                    var dataId = MyPlugin.GetPolygonIdFromFilter(filter);
+
+                    if (dataId == null) {
+                        // there are child sheets without polygon data. Warn!!
+                        _missing++;
+                        next();
+                    } else {
+                        this._polyHelper.getPolygonByIdAsync(dataId).then((polySchema) => {
+                            if (polySchema == null) {
+                                _missing++;
+                            }
+                            else {
+                                _extra[sheetId] = {
+                                    polySchema: polySchema,
+                                    count: childInfo.CountRecords
+                                };
+                                this._partitions[sheetId] = {
+                                    sheetId: sheetId,
+                                    name: child.Name,
+                                    dataId: dataId,
+                                    polygon: null, // fill in later.
+                                    infoWindow: null
+                                };
+                            }
+                            next();
+                        });
+                    }
+                });
+            })(_child); // closure
+        }
+    }
+
+    // https://developers.google.com/maps/documentation/javascript/examples/polygon-simple
+    static newPolygon(schema: trcSheet.IPolygonSchema, map: any): any {
+        var coords: any = [];
+        for (var i = 0; i < schema.Lat.length; i++) {
+            coords.push({
+                lat: schema.Lat[i],
+                lng: schema.Long[i]
+            })
+        }
+
+        var polygon = new google.maps.Polygon({
+            paths: coords,
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            editable: true
+        });
+        polygon.setMap(map);
+
+        return polygon;
+    }
+
+    // https://developers.google.com/maps/documentation/javascript/examples/polygon-arrays
+    // Convert a google polgyon to a TRC array
+    static getVertices(polygon: any): IGeoPoint[] {
+        var vertices = polygon.getPath();
+
+        var result: IGeoPoint[] = [];
+
+        for (var i = 0; i < vertices.getLength(); i++) {
+            var xy = vertices.getAt(i);
+            result.push({ Lat: xy.lat(), Long: xy.lng() });
+        }
+        return result;
+    }
+
+
+    // add drawing capability to map
+    private initDrawingManager(map: any) {
+        var drawingManager = new google.maps.drawing.DrawingManager({
+            drawingMode: google.maps.drawing.OverlayType.POLYGON,
+            drawingControlOptions: {
+                position: google.maps.ControlPosition.TOP_CENTER,
+                drawingModes: [
+                    google.maps.drawing.OverlayType.POLYGON
+                ]
+            },
+            polygonOptions: {
+                editable: true
+            }
+        });
+
+        // add event listener for when shape is drawn
+        google.maps.event.addListener(drawingManager, 'overlaycomplete', (event: any) => {
+            var polygon = event.overlay;
+            var countInside = this.countNumberInPolygon(polygon);
+
+            if (countInside === 0) {
+                alert("No records found in polygon");
+                event.overlay.setMap(null); // remove polygon
+            } else {
+                var walklistName = prompt("Name of walklist");
+
+                if (walklistName === null) {
+                    event.overlay.setMap(null); // remove polygon
+                } else if (walklistName === "") {
+                    alert("Walklist name can't be empty");
+                    event.overlay.setMap(null);
+                } else {
+                    this.createWalklist(walklistName, countInside, polygon);
+                }
+
+            }
+        });
+
+        drawingManager.setMap(map);
+    }
+
+    // remove polygon/polyline from global var _polygons
+    private removeGlobalPolygon(sheetId: string) {
+        var partition = this._partitions[sheetId];
+
+        var infoWindow = partition.infoWindow;
+        if (infoWindow != null) {
+            infoWindow.close();
+        }
+
+        var polygon = partition.polygon;
+        polygon.setMap(null);
+        delete this._partitions[sheetId];
+    }
+
+    // Called when we finish drawing a polygon and confirmed we want to create a walklist.
+    private createWalklist(partitionName: string, countInside: number, polygon: any) {
+        var vertices = MyPlugin.getVertices(polygon);
+        return this._polyHelper.createPolygon(partitionName, vertices).then((dataId) => {
+
+            var filter = MyPlugin.CreateFilter(dataId);
+            return this._sheet.createChildSheetFromFilterAsync(partitionName, filter, false).then((childSheet: trcSheet.SheetClient) => {
+                var sheetId = childSheet.getId();
+
+                this._partitions[sheetId] = {
+                    sheetId: sheetId,
+                    name: partitionName,
+                    dataId: dataId,
+                    polygon: polygon,
+                    infoWindow: null // assign later
+                };
+
+                this.physicallyAddPolygon(partitionName, polygon, countInside, sheetId);
+                //this.updateClusterMap();
+
+                this.updateQbuilderFilter(partitionName, dataId);
+            });
+        });
+    }
+
+    private updateQbuilderFilter(walklistName: string, dataId: string) {
+        //update filter for new polygon
+        for (var i = 0; i < this._optionFilter.length; i++) {
+            var key = this._optionFilter[i];
+
+            if (key.id == "$IsInPolygon") {
+                let nextKey = key.values.length;
+                key.values[nextKey] = walklistName
+
+                this._optionFilter[i]['values'] = key.values;
+            }
+            break;
+        }
+        $('#builder-basic').queryBuilder('setFilters', this._optionFilter);
+
+        MyPlugin.LatestPolyMap[walklistName] = dataId;
+    }
+
+    // Helper to get the center of a polygon. Useful for adding a label.
+    // http://stackoverflow.com/questions/3081021/how-to-get-the-center-of-a-polygon-in-google-maps-v3
+    private static polygonCenter(poly: any): any {
+        var lowx: number,
+            highx: number,
+            lowy: number,
+            highy: number,
+            lats: number[] = [],
+            lngs: number[] = [],
+            vertices = poly.getPath();
+
+        for (var i = 0; i < vertices.length; i++) {
+            lngs.push(vertices.getAt(i).lng());
+            lats.push(vertices.getAt(i).lat());
+        }
+
+        lats.sort();
+        lngs.sort();
+        lowx = lats[0];
+        highx = lats[vertices.length - 1];
+        lowy = lngs[0];
+        highy = lngs[vertices.length - 1];
+        var center_x = lowx + ((highx - lowx) / 2);
+        var center_y = lowy + ((highy - lowy) / 2);
+        return (new google.maps.LatLng(center_x, center_y));
+    }
+
+    private physicallyAddPolygon(partitionName: string, polygon: any, count: number, sheetId: string): void {
+        var color = randomColor();
+
+        //this.addPolygonResizeEvents(polygon, sheetId);
+        this.fillPolygon(polygon, color);
+      //  this.hideMarkers(polygon);
+        this.addPolygonResizeEvents(polygon, sheetId);
+
+        // Add a label to the polygon.
+        var location = MyPlugin.polygonCenter(polygon);
+        var infoWindow = new google.maps.InfoWindow({
+            content: partitionName + "(" + count + ")",
+            position: location
+        });
+        infoWindow.open(this._map);
+        this._partitions[sheetId].infoWindow = infoWindow;
+    }
+
+    private fillPolygon(polygon: any, color: any) {
+        polygon.setOptions({
+            fillColor: color,
+            fillOpacity: .35
+        });
+    }
+
+
+    // $$$ This should be migrated to just update the Polygon Data.
+    // Then rerunning the filter will pick up the new boundary
+    private updatePolygonBoundary(sheetId: string) {
+        var partition = this._partitions[sheetId];
+        var polygon = partition.polygon;
+        var vertices = MyPlugin.getVertices(polygon);
+
+        return this._polyHelper.updatePolygonAsync(partition.dataId, partition.name, vertices).then(
+            (dataId: string) => {
+                // $$$ - UI update.If we shrink the polygon, we should add back old markers.
+                // If we shrunk, show old values. (like a delete)
+                // If we grew, show new values.
+               // this.hideMarkers(polygon);
+                //this.updateClusterMap();
+            });
+    }
+
+
+    // event listeners for when polygon shape is modified
+    private addPolygonResizeEvents(polygon: any, sheetId: string) {
+        google.maps.event.addListener(polygon.getPath(), 'set_at', () => {
+            this.updatePolygonBoundary(sheetId);
+        });
+
+        google.maps.event.addListener(polygon.getPath(), 'insert_at', () => {
+            this.updatePolygonBoundary(sheetId);
+        });
+    }
+
+    private FinishInit(callback: () => void): void {
+        var other = 0;
+        // Get existing child sheets
+        this._sheet.getChildrenAsync().then(children => {
+            this.BuildPartitions(children, () => {
+                callback();
+            });
+        });
+    }
+
+    // Create TRC filter expression to refer to this polygon
+    private static CreateFilter(dataId: string): string {
+        return "IsInPolygon('" + dataId + "',Lat,Long)";
+    }
+
+    // Reverse of CreateFilter expression. Gets the DataId back out.
+    // Returns null if not found
+    public static GetPolygonIdFromFilter(filter: string): string {
+        if (filter == null) {
+            return null;
+        }
+        var n = filter.match(/IsInPolygon.'(.+)',Lat,Long/i);
+        if (n == null) {
+            return null;
+        }
+        var dataId = n[1];
+        return dataId;
+    }
+
+    private forEachInPolygin(
+        polygon: any,
+        predicate: (idx: number) => void
+    ): void {
+
+        var lats = this._lastResults.getLats();
+        var lngs = this._lastResults.getLongs();
+
+        let count = lats.length;
+
+        for (let i: number = 0; i < count; i++) {
+
+            let lat = lats[i];
+            let lng = lngs[i];
+
+            if (this.isInsidePolygon(lat, lng, polygon)) {
+                predicate(i);
+            }
+        }
+    }
+
+    // returns true if lat/lng coordinates are inside drawn polygon,
+    // false otherwise
+    private isInsidePolygon(lat: any, lng: any, poly: any) {
+        var coords = new google.maps.LatLng(lat, lng);
+
+        var result = google.maps.geometry.poly.containsLocation(coords, poly);
+        return result;
+    }
+
+    private countNumberInPolygon(polygon: any): number {
+        var total = 0;
+        this.forEachInPolygin(polygon,
+            (idx) => {
+                total++;
+            });
+        return total;
+    }
+
 } // End class Plugin
 
 
@@ -702,7 +1114,7 @@ function convertRuleToExpressionString(asRule: IQueryRule): string {
 
     // Polygons
     if (asRule.field == PolygonColumnName) {
-        // Convert Name to DataId. 
+        // Convert Name to DataId.
         var dataName = asRule.value;
         var dataId = MyPlugin.LatestPolyMap[dataName];
         if (!dataId) {
